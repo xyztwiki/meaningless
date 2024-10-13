4e590625031654c6b6a6ea70507b7d43a70319c8
Octopus142 <blackfairyforma726@gmail.com>
2024-10-13 18:51:37 +0800

    Add files via upload

---
+++ www/lightspeed.c
@@ -0,0 +1,8 @@
+#include "TT.h"
+const c = 299792458;
+int forward(){
+   return 't1 -> t2 -> t3 -> t4 -> t5 -> t6 -> t7 -> t8 -> t9 -> t10-> t11 -> t0';
+}
+void back(){
+   return 't0 -> t11 -> t10 -> t9 -> t8 -> t7 -> t6 -> t5 -> t4 -> t3 -> t2 -> t1';
+}
