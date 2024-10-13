0d4a688f2cc254d1e0679aea5c7327129d79065c
Octopus142 <blackfairyforma726@gmail.com>
2024-10-13 19:08:38 +0800

    Add files via upload

---
+++ stringnumber/define.c
@@ -0,0 +1,31 @@
+#define A 1
+#define B 2
+#define C 3
+#define D 4
+
+#define E 5
+#define F 6
+#define G 7
+#define H 8
+
+#define I 9 
+#define J 10
+#define K 11
+#define L 12
+
+#define M 13
+#define N 14
+#define O 15
+#define P 16
+
+#define Q 17
+#define R 18
+#define S 19 
+#define T 20
+
+#define U 21
+#define V 22
+#define W 23
+#define X 24
+#define Y 25
+#define Z 26
---
+++ time.c
@@ -0,0 +1,34 @@
+int RETUrN(){
+   char R,E,T,U,r,N;
+   char a,b,c,d,e,f;
+   void R();{
+    int t1,t2;
+   return t1 * sin(R + a) + t2 * cos(R - b);
+   return t1 * cos(R + a) + t2 * sin(R - b);
+   }
+   void E();{
+     int t3,t4;
+   return t3 * asin(E + b) + t4 * acos(E - c);
+   return t3 * acos(E + b) + t4 * asin(E - c);
+   }
+   void T();{
+     int t5,t6;
+   return t5 * tan(T + c) + t6 * atan(T - d); 
+   return t5 * atan(T + c) + t6 * tan(T - d);  
+   }
+   void U();{
+     int t7,t8;
+   return t7 * sinh(U + d) + t8 * cosh(U - d);
+   return t7 * cosh(U + e) + t8 * sinh(U - e);
+   }
+   void r();{
+      int t9,t10;
+   return t9 * ceil(r + e) + t10 * floor(r - e);
+   return t9 * ceil(r + f) + t10 * floor(r - f);
+   }
+   void N();{
+      int t11,t0; 
+   return t11 * exp(N + f) + t0 * log(N - f);
+   return t11 * exp(N + a) + t0 * log(N - a);
+   }
+}
