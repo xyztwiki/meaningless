aca63e22e3fe954635a9d8869e2c2619b7d54b91
Octopus142 <blackfairyforma726@gmail.com>
2024-10-20 22:28:11 +0800

    Update time.c

--- time.c
+++ time.c
@@ -3,33 +3,33 @@ int RETUrN(){
    char R,E,T,U,r,N;
    char a,b,c,d,e,f;
    void R();{
-    int t1,t2;
-   return t1 * sin(R + a) + t2 * cos(R - b);
-   return t1 * cos(R + a) + t2 * sin(R - b);
+    int T1,T2;
+   return T1 * sin(R + a) + T2 * cos(R - b);
+   return T1 * cos(R + a) + T2 * sin(R - b);
    }
    void E();{
-     int t3,t4;
-   return t3 * asin(E + b) + t4 * acos(E - c);
-   return t3 * acos(E + b) + t4 * asin(E - c);
+     int T3,T4;
+   return T3 * asin(E + b) + T4 * acos(E - c);
+   return T3 * acos(E + b) + T4 * asin(E - c);
    }
    void T();{
-     int t5,t6;
-   return t5 * tan(T + c) + t6 * atan(T - d); 
-   return t5 * atan(T + c) + t6 * tan(T - d);  
+     int T5,T6;
+   return T5 * tan(T + c) + T6 * atan(T - d); 
+   return T5 * atan(T + c) + T6 * tan(T - d);  
    }
    void U();{
-     int t7,t8;
-   return t7 * sinh(U + d) + t8 * cosh(U - d);
-   return t7 * cosh(U + e) + t8 * sinh(U - e);
+     int T7,T8;
+   return T7 * sinh(U + d) + T8 * cosh(U - d);
+   return T7 * cosh(U + e) + T8 * sinh(U - e);
    }
    void r();{
-      int t9,t10;
-   return t9 * ceil(r + e) + t10 * floor(r - e);
-   return t9 * ceil(r + f) + t10 * floor(r - f);
+      int T9,T10;
+   return T9 * ceil(r + e) + T10 * floor(r - e);
+   return T9 * ceil(r + f) + T10 * floor(r - f);
    }
    void N();{
-      int t11,t0; 
-   return t11 * exp(N + f) + t0 * log(N - f);
-   return t11 * exp(N + a) + t0 * log(N - a);
+      int T11,T0; 
+   return T11 * exp(N + f) + T0 * log(N - f);
+   return T11 * exp(N + a) + T0 * log(N - a);
    }
 }
